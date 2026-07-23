3663c7ed4dfc1ca16490bd5eae36cc49b38788ac
Christian Julias <cjulias@catalyte.io>
2026-07-23 12:40:43 -0500

    Created get_all function for get request

--- src/main.py
+++ src/main.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI
 
+from src.models.product import Product
+from src.repositories.product_repository import ProductRepository
+
 app = FastAPI()
+
+product_repository = ProductRepository()
+product_repository.add_product(
+    Product(
+        name="soil",
+        unit="bag",
+        cost_per_unit=3.5,
+        price_per_unit=5.5,
+        quantity_in_stock=10,
+    )
+)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello, {name}!"}
+
+
+@app.get("/products")
+async def get_products():
+    return product_repository.get_all()
---
+++ src/repositories/product_repository.py
@@ -0,0 +1,13 @@
+from src.models.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self._products: list[Product] = []
+
+    def add_product(self, product: Product) -> None:
+        self._products.append(product)
+
+    def get_all(self) -> list[Product]:
+        return self._products
+    
