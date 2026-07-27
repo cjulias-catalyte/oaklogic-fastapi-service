856eb570919eb7b5a9cd1e397ee5473b76f80525
Sabah <asabah@catalyte.io>
2026-07-27 15:26:26 -0500

    v

--- src/repositories/product_repository.py
+++ src/repositories/product_repository.py
@@ -2,59 +2,11 @@ from src.models.product import Product
 
 
 class ProductRepository:
-    # def __init__(self):
-    #     self._products: list[Product] = []
-
-    # def add_product(self, product: Product) -> None:
-    #     self._products.append(product)
-
-   
-
-    def __init__(self, db: Session):
-        self.db = db
-    
-    def create_new_product(self, product_data: ProductSchema) -> Product:
-        db_product = Product(
-            id=product_data.id,
-            name=product_data.name,
-            unit=product_data.unit,
-            cost_per_unit=product_data.cost_per_unit,
-            price_per_unit=product_data.price_per_unit,
-            quantity_in_stock=product_data.quantity_in_stock,
-        )
-
-        self.db.add(db_product)
-        self.db.commit()
-        self.db.refresh(db_product)
-
-        return db_product
-
-    def get_all_products(self) -> list[Product]:
-        return self.db.query(Product).all()
+    def __init__(self):
+        self._products: list[Product] = []
 
     def add_product(self, product: Product) -> None:
         self._products.append(product)
 
-    def get_product_by_id(
-        self,
-        product_id: int,
-    ) -> Product | None:
-        return (
-            self.db.query(Product)
-            .filter(Product.id == product_id)
-            .first()
-        )
-
-    def delete_product(
-        self,
-        product_id: int,
-    ) -> bool:
-        product = self.get_product_by_id(product_id)
-
-        if product is None:
-            return False
-
-        self.db.delete(product)
-        self.db.commit()
-
-        return True
+    def get_all(self) -> list[Product]:
+        return self._products
